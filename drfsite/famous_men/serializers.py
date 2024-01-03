a8d7c307ec251be9fbd68a9c46248f7298a3cf05
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-01-03 03:57:10 +0300

    Updated: Serialization

--- drfsite/famous_men/serializers.py
+++ drfsite/famous_men/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from .models import FamousMen
 
 
-class FamousMenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FamousMen
-        fields = ('title', 'category_id')
+class FamousMenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
--- drfsite/famous_men/views.py
+++ drfsite/famous_men/views.py
@@ -1,8 +1,23 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import FamousMen
 from .serializers import FamousMenSerializer
 
 
-class FamousMenAPIView(generics.ListAPIView):
-    queryset = FamousMen.objects.all()
-    serializer_class = FamousMenSerializer
+class FamousMenAPIView(APIView):
+    def get(self, request):
+        w = FamousMen.objects.all()
+        return Response({'posts': FamousMenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = FamousMenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = FamousMen.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+
+        return Response({'post': FamousMenSerializer(post_new).data})
