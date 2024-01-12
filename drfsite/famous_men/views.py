b7a90cfcbb0b9e956e9ee160c949e3aa03be5b9a
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-01-12 16:37:02 +0300

    Added: Pagination

--- drfsite/famous_men/views.py
+++ drfsite/famous_men/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import FamousMen
@@ -6,10 +7,17 @@ from .permissions import IsAdminOrReadOnly
 from .serializers import FamousMenSerializer
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 2
+
+
 class FamousMenAPIList(generics.ListCreateAPIView):
     queryset = FamousMen.objects.all()
     serializer_class = FamousMenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 
 class FamousMenAPIUpdate(generics.RetrieveUpdateAPIView):
