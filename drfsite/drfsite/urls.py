98560d3f82ecfa8a2e62978989c9853b77ebf08b
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-01-12 16:10:57 +0300

    Added: Authorization using JWT

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 
 from famous_men.views import FamousMenAPIList, FamousMenAPIUpdate, FamousMenAPIDestroy
 
@@ -27,4 +28,7 @@ urlpatterns = [
     path('api/v1/famous-men-delete/<int:pk>/', FamousMenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
